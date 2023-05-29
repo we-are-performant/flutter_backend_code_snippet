dd41f48597ed88329e6016644446a2772c44c48e
aftabale <38526273+aftabale@users.noreply.github.com>
2023-05-29 16:34:30 +0500

    Add files via upload

---
+++ models/address_model.dart
@@ -0,0 +1,41 @@
+
+import 'dart:convert' show json;
+
+List<Address> addressFromJson(String str) => List<Address>.from(json.decode(str).map((x) => Address.fromJson(x)));
+class Address {
+  int? addId;
+  int? userId;
+  String? address;
+  String? streetNo;
+  String? appartmentNo;
+  String? lattitude;
+  String? longitude;
+  bool? primaryAddress = false;
+
+  Address(
+      {this.addId,this.userId,this.address, this.streetNo, this.appartmentNo, this.lattitude, this.longitude,this.primaryAddress});
+
+  Address.fromJson(Map<String, dynamic> json) {
+    addId = json['add_id'];
+    userId = json['user_id'];
+    address = json['address'];
+    streetNo = json['street_no'];
+    appartmentNo = json['appartment_no'];
+    lattitude = json['latitude'];
+    longitude = json['longitude'];
+    primaryAddress =((json['primary_address']) == 1)?true:false;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['add_id'] = addId;
+    data['user_id'] = userId;
+    data['address'] = address;
+    data['street_no'] = streetNo;
+    data['appartment_no'] = appartmentNo;
+    data['latitude'] = lattitude;
+    data['longitude'] = longitude;
+    data['primary_address'] = primaryAddress;
+    return data;
+  }
+}
