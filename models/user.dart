fd19c55b9f0330a05c86d43f70d693f3e710e279
aftabale <38526273+aftabale@users.noreply.github.com>
2023-05-29 16:29:08 +0500

    user.dart

---
+++ models/user.dart
@@ -0,0 +1,35 @@
+class User {
+  int? userId;
+  String? firstName;
+  String? lastName;
+  String? contactNo;
+  String? email;
+  String? deviceToken;
+  int? emailVerification;
+  String? lang;
+
+  User(
+      {this.userId, this.firstName, this.lastName, this.contactNo, this.email,this.deviceToken,this.emailVerification});
+
+  User.fromJson(Map<String, dynamic> json) {
+    userId = json['user_id'];
+    firstName = json['first_name'];
+    lastName = json['last_name'];
+    contactNo = json['contact_no'];
+    email = json['email'];
+    emailVerification = json['email_verified'];
+    lang = json['lang'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['user_id'] = userId;
+    data['first_name'] = firstName;
+    data['last_name'] = lastName;
+    data['contact_no'] = contactNo;
+    data['email'] = email;
+    data['email_verified'] = emailVerification;
+    data['lang'] = lang;
+    return data;
+  }
+}
